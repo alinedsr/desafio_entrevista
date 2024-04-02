289725587f1f3c1a433b5b582c675ebe5347c216
Aline Silveira <aline@outlook.ie>
2024-04-01 22:34:40 -0300

    build ambiente

---
+++ main.py
@@ -0,0 +1,6 @@
+# Desafio - Entrevista
+
+nome = input("Olá! Como você se chama? ")
+print(f"Seja bem-vindo, {nome}. Seu nome possui {len(nome)} letras.")
+idade = int(input("Qual a sua idade? "))
+print(f"Legal! Em cinco anos, você terá {idade + 5} anos.")
